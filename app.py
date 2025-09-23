3ff3856c9d4b55a75211d975c1ca98c48a1aaa52
M27113 <manasa02k@gmail.com>
2025-09-23 20:50:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import re
 
 from planner import extract_location, generate_plan
 from db import init_db, save_plan, fetch_plans, clean_plan_output
-import warnings
-warnings.filterwarnings("ignore")
 
 # ------------------ Setup ------------------
 st.set_page_config(page_title="AI Task Planner", page_icon="📝", layout="wide")
@@ -141,4 +139,5 @@ else:
                 file_name=f"task_plan_{pid}.pdf",
                 mime="application/pdf"
             )
+
 
